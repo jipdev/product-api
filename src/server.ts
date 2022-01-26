d744a0325f425a5bd80a1912ab9d4dac51aeee9b
Jhonattan Loli Mazzuco <jhonattanloli@gmail.com>
2022-01-26 01:25:07 +0044

    Adicionado cors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
+import cors from 'cors';
 import express from 'express';
 import Routes from './routes/routes';
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 app.use(express.json());
 
